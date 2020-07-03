7c2fb1003f999f5b972ff98cb2731f0ee1977226
Pyx920926 <pengyaxiong0926@gmail.com>
2020-07-03 11:18:20 +0800

    days

--- app/Admin/Controllers/ProjectController.php
+++ app/Admin/Controllers/ProjectController.php
@@ -62,7 +62,7 @@ class ProjectController extends AdminController
         $grid->column('node', __('Node'))->display(function ($node) {
             $html=[];
             foreach ($node as $k=>$v){
-                if(!isset($v["staff_id"])){
+                if(!isset($v["staff_id"]) || !isset($v["days"])){
                     continue;
                 }
                 $name=Staff::find($v["staff_id"])->name;
