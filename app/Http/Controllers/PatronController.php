1f611f08098f91494352a305e32bb06d67c8426f
Pyx920926 <pengyaxiong0926@gmail.com>
2020-09-07 18:40:11 +0800

    客户手机号不用必填

--- app/Http/Controllers/PatronController.php
+++ app/Http/Controllers/PatronController.php
@@ -95,7 +95,6 @@ class PatronController extends Controller
         $messages = [
             'company_name.required' =>'公司名称不能为空',
             'name.required' => '联系人不能为空',
-            'phone.required' => '联系人电话不能为空',
             'phone.unique' => '联系人已经存在',
             'job.required' => '联系人职位不能为空',
             'need.required' => '需求不能为空',
@@ -103,7 +102,7 @@ class PatronController extends Controller
         $rules = [
             'company_name' => 'required',
             'name' => 'required',
-            'phone' => 'required|unique:wechat_patron,phone,'.$patron->id,
+            'phone' => 'unique:wechat_patron,phone,'.$patron->id,
             'job' => 'required',
             'need' => 'required',
         ];
