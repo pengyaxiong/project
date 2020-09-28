082467f7bd27ed6e4b99fcd3cc22f731e1347b8b
Pyx920926 <pengyaxiong0926@gmail.com>
2020-09-28 10:22:25 +0800

    0.0

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -7,6 +7,7 @@ use App\Models\Notice;
 use Illuminate\Http\Request;
 use App\Handlers\WechatConfigHandler;
 use Illuminate\Support\Facades\Auth;
+
 class WelcomeController extends Controller
 {
     private $wechat;
@@ -24,14 +25,16 @@ class WelcomeController extends Controller
     {
         $notices = Notice::wherein('department_id', [0, 12])->orderby('sort_order')->get();
 
-        $app = $this->wechat->app();
-        $user = session('wechat.oauth_user.default'); //拿到授权用户资料
-        $original = $user->original;
-        $openid = $original['openid'];
-        if ($openid!=null){
-            $customer = Customer::where('openid', $openid)->first();
-            if ($customer) {
-                Auth::loginUsingId($customer->id);
+        if (is_wei_xin()) {
+            $app = $this->wechat->app();
+            $user = session('wechat.oauth_user.default'); //拿到授权用户资料
+            $original = $user->original;
+            $openid = $original['openid'];
+            if ($openid != null) {
+                $customer = Customer::where('openid', $openid)->first();
+                if ($customer) {
+                    Auth::loginUsingId($customer->id);
+                }
             }
         }
         return view('welcome', compact('notices'));
